2475313c2a3c62f4cbca57ae246f5e4763e377d3
Daniel Alegre <dalegr1@lsu.edu>
2020-05-06 22:05:49 -0500

    commiting

--- TicketSystem/Form1.cs
+++ TicketSystem/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
 
 namespace TicketSystem
 {
@@ -22,35 +24,59 @@ namespace TicketSystem
             Ticket obj = ticketBindingSource.Current as Ticket;
             if (obj != null)
             {
-                using(frmTicketInfo frm = new frmTicketInfo(obj))
+                using (frmTicketInfo frm = new frmTicketInfo(obj))
                 {
                     frm.ShowDialog();
                 }
             }
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+           
+            using (var reader = new StreamReader("Tickets.txt"))
+            {
+                List<string> Name = new List<string>();
+                List<string> DefectType = new List<string>();
+                List<string> Date = new List<string>();
+                List<string> Time = new List<string>();
+                List<string> Batch = new List<string>();
+                List<string> Station = new List<string>();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    Name.Add(values[0]);
+                    DefectType.Add(values[1]);
+                    Date.Add(values[2]);
+                    Time.Add(values[3]);
+                    Batch.Add(values[4]);
+                    Station.Add(values[5]);
+
+                }
+                for (int x = 0; x < File.ReadLines("Tickets.txt").Count(); x++)
+                    ticketBindingSource.Add(new Ticket()
+                    {
+                        Name = Name[x],
+                        DefectType = DefectType[x],
+                        Date = Convert.ToDateTime(Date[x]),
+                        Time = Convert.ToDateTime(Time[x]),
+                        Batch = Convert.ToInt32(Batch[x]),
+                        Station = Convert.ToInt32(Station[x])
+                    });
+            }
+            /*
             ticketBindingSource.Add(new Ticket()
             {
-                Name = "Daniel",
-                DefectType = "Security Defect",
+                Name = "N/A",
+                DefectType = "New Ticket",
                 Date = Convert.ToDateTime("01/01/2000"),
-                Time = Convert.ToDateTime("12:00 PM"),
-                Batch = 3,
-                Station = 17
-            });
-            ticketBindingSource.Add(new Ticket()
-            {
-                Name = "Bob",
-                DefectType = "Other",
-                Date = Convert.ToDateTime("05/22/2012"),
-                Time = Convert.ToDateTime("4:00 PM"),
-                Batch = 12,
-                Station = 6
-            });
+                Time = Convert.ToDateTime("00:00"),
+                Batch = Convert.ToInt32("0"),
+                Station = Convert.ToInt32("0")
+            });*/
         }
-
-
     }
 }
